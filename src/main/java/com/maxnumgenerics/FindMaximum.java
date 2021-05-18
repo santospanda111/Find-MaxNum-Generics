7d2adeec07bd96561f40eab57a4dd220fd920aee
santospanda111 <santospanda111@gmail.com>
2021-05-18 22:29:55 +0530

    Refactored the code and test cases also.

--- src/main/java/com/maxnumgenerics/FindMaximum.java
+++ src/main/java/com/maxnumgenerics/FindMaximum.java
@@ -2,59 +2,23 @@ package com.maxnumgenerics;
 
 public class FindMaximum {
     /**
-     * here i have created a parameterised constructor having given parameters.
-     * First i have taken numberOne is maxNumber
-     * then i used compareTo method which will compare with numberTwo..and gradually compare with numberThree also.
-     * @param numberOne
-     * @param numberTwo
-     * @param numberThree
-     * @return maxNumber
-     */
-    public int findMaxNumber(Integer numberOne, Integer numberTwo, Integer numberThree) {
-        Integer maxNumber = numberOne;
-        if (numberTwo.compareTo(maxNumber) > 0) {
-            maxNumber = numberTwo;
-        }
-        if (numberThree.compareTo(maxNumber) > 0) {
-            maxNumber = numberThree;
-        }
-        return maxNumber;
-    }
-
-    /**
-     * As above it'll work the same but having only float values.
-     * @param numberOne
-     * @param numberTwo
-     * @param numberThree
+     * here i have used Generic functions.
+     * here E is the type of data.
+     * Gradually it will return the max value.
+     * @param firstValue
+     * @param secondValue
+     * @param thirdValue
+     * @param <E>
      * @return
      */
-
-    public float findFloatMaxNumber(Float numberOne, Float numberTwo, Float numberThree) {
-        Float maxNumber = numberOne;
-        if (numberTwo.compareTo(maxNumber) > 0) {
-            maxNumber = numberTwo;
+    public <E extends Comparable<E>> E findMax(E firstValue, E secondValue, E thirdValue) {
+        E maxValue = firstValue;
+        if (secondValue.compareTo(maxValue) > 0) {
+            maxValue = secondValue;
         }
-        if (numberThree.compareTo(maxNumber) > 0) {
-            maxNumber = numberThree;
+        if (thirdValue.compareTo(maxValue) > 0) {
+            maxValue = thirdValue;
         }
-        return maxNumber;
-    }
-    /**
-     * As above it'll work the same but having only String values.
-     * @param numberOne
-     * @param numberTwo
-     * @param numberThree
-     * @return
-     */
-
-    public String findMaxString(String numberOne, String numberTwo, String numberThree) {
-        String maxString = numberOne;
-        if (numberTwo.compareTo(maxString) > 0) {
-            maxString = numberTwo;
-        }
-        if (numberThree.compareTo(maxString) > 0) {
-            maxString = numberThree;
-        }
-        return maxString;
+        return maxValue;
     }
 }
--- src/test/java/com/maxnumgenerics/FindMaxTest.java
+++ src/test/java/com/maxnumgenerics/FindMaxTest.java
@@ -4,63 +4,57 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FindMaxTest {
-    /**
-     * here i have created an object of FindMaximum class.
-     * created three test cases which will check and give the test case result.
-     */
-    FindMaximum findMax = new FindMaximum();
-    @Test
-    void ifNumberOneIsMaxReturnTrue() {
-        Assertions.assertEquals(100, findMax.findMaxNumber(100,50,25));
-    }
-
-    @Test
-    void ifNumberTwoIsMaxReturnTrue() {
-        Assertions.assertEquals(100, findMax.findMaxNumber(50, 100, 25));
-    }
-
-    @Test
-    void ifNumberThreeIsMaxReturnTrue() {
-        Assertions.assertEquals(100, findMax.findMaxNumber(25, 50, 100));
-    }
+    FindMaximum findMaxObj = new FindMaximum();
 
     /**
-     * Similarly as above i have written test cases
-     * created three test cases which will check and give the test case result.
+     * Here we have the same code only i have used E as a type.
+     * that E will automatically fetch any datatypes.
      */
+
+    @Test
+    void ifIntegerNumberOneIsMaxReturnTrue() {
+
+        Assertions.assertEquals(100, findMaxObj.findMax(100, 50, 25));
+    }
+
+    @Test
+    void ifIntegerNumberTwoIsMaxReturnTrue() {
+
+        Assertions.assertEquals(100, findMaxObj.findMax(50, 100, 25));
+    }
+
+    @Test
+    void ifIntegerNumberThreeIsMaxReturnTrue() {
+        Assertions.assertEquals(100, findMaxObj.findMax(25, 50, 100));
+    }
 
     @Test
     void ifFloatNumberOneIsMaxReturnTrue() {
-        Assertions.assertEquals(10.0, findMax.findFloatMaxNumber(10.0F, 5.0F, 2.5F));
+        Assertions.assertEquals(10.0F, findMaxObj.findMax(10.0F, 5.0F, 2.5F));
     }
 
     @Test
     void ifFloatNumberTwoIsMaxReturnTrue() {
-        Assertions.assertEquals(10.0, findMax.findFloatMaxNumber(5.0F, 10.0F, 2.5F));
+        Assertions.assertEquals(10.0F, findMaxObj.findMax(5.0F, 10.0F, 2.5F));
     }
 
     @Test
     void ifFloatNumberThreeIsMaxReturnTrue() {
-        Assertions.assertEquals(10.0, findMax.findFloatMaxNumber(2.5F, 5.0F, 10.0F));
-    }
-    /**
-     * Similarly as above i have written test cases
-     * created three test cases which will check and give the test case result.
-     * Here i have taken String as values.
-     */
-
-    @Test
-    void ifStringNumberOneIsMaxReturnTrue() {
-        Assertions.assertEquals("peach", findMax.findMaxString("banana", "peach", "apple"));
+        Assertions.assertEquals(10.0F, findMaxObj.findMax(2.5F, 5.0F, 10.0F));
     }
 
     @Test
-    void ifStringNumberTwoIsMaxReturnTrue() {
-        Assertions.assertEquals("peach", findMax.findMaxString("apple", "banana", "peach"));
+    void ifStringOneIsMaxReturnTrue() {
+        Assertions.assertEquals("peach", findMaxObj.findMax("banana", "apple", "peach"));
     }
 
     @Test
-    void ifStringNumberThreeIsMaxReturnTrue() {
-        Assertions.assertEquals("peach", findMax.findMaxString("peach", "apple", "banana"));
+    void ifStringTwoIsMaxReturnTrue() {
+        Assertions.assertEquals("peach", findMaxObj.findMax("apple", "banana", "peach"));
+    }
+
+    @Test
+    void ifStringThreeIsMaxReturnTrue() {
+        Assertions.assertEquals("peach", findMaxObj.findMax("peach", "apple", "banana"));
     }
 }
